b3690403764fedb2c141ff637be86b892bf65560
roomy10 <shadowconqueror2@gmail.com>
2025-09-11 22:42:38 +0800

    flow

--- code.py
+++ code.py
@@ -1,5 +1,31 @@
 #Laboratory Tracker CS5 1st Quarter Project
 
+'''
+Flow
+
+-- User can end or backtrack transaction at any point (need function)
+-- Record transactions that happened in the day ie. current session. 
+-- User can input "standing" to check current standing of all transactions
+-- End with a receipt showing which lab and equipment are reserved (ex. cs_lab1 -- 1st period, previous quanitity >> current quantity)
+
+Command (Which lab for transaction) --> if
+
+--> cs lab --> lab transaction (Reservation) --> 
+cs_lab_reservations list (in periods, 10 periods total, excluding wellness and break period) --> 
+who's reserving, which lab, period, and the reason for use --> prompt to record another transaction (loop back)
+
+
+--> bio lab --> Reservation (in periods, 10 periods total, excluding wellness and break period) 
+--> lab transaction (Access available equipment/Borrowing equipment from the stockroom/Add or Returning of borrowed equipment/NA)
+--> who's reserving, which lab, period, and the reason for use --> prompt to record another transaction (loop back)
+
+
+--> chem lab--> Reservation (in periods, 10 periods total, excluding wellness and break period) 
+--> lab transaction (Access available equipment/Borrowing equipment from the stockroom/Add or Returning of borrowed equipment/NA)
+--> who's reserving, which lab, period, and the reason for use --> prompt to record another transaction (loop back)
+
+ 
+'''
 
 #Initializiation of Variables
 chem=["Test Tubes", "Gloves", "Flasks", "Beakers"]
