678b75f2d20278a2815eec5ffd9c89552c6343a5
El1jah13 <144795387+El1jah13@users.noreply.github.com>
2025-09-14 18:27:42 +0800

    Update code.py
    I added some extra details and messages to the code and added the nested lists

--- code.py
+++ code.py
@@ -28,55 +28,79 @@ who's reserving, which lab, period, and the reason for use --> prompt to record 
 '''
 
 #Initializiation of Variables
-chem=["Test Tubes", "Gloves", "Flasks", "Beakers"]
-avail=[10,5,5,3]
+materials=[["Test Tubes", "Gloves", "Flasks", "Beakers"],["Bio stuff"]]
+avail=[[10,5,5,3],[1]]
 
 
 #Functions
-def access(chem,avail):
-  i=0
-  while i<len(chem):
-    print(chem[i]+" - "+str(avail[i]))
-    i+=1
+def access(materials,avail):
+    id=1
+    print("Id:  Item and availability:")
+    while id-1<len(materials):
+        print(str(id)+".    "+materials[id-1]+" - "+str(avail[id-1]))
+        id+=1
+
 def take(avail):
-  while True:
-    id=input("Enter the id of the material you want: ")
-    if id.lower()=="exit":
-      break
-    else:
-      id=int(id)
-      i=id-1
-      while True:
-        amt=int(input("Enter how many you want to take: "))
-          if amt>avail[i]:
-            print("Not enough.")
-          else:
-            avail[i]-=amt
+    while True:
+        id=input("Enter the id of the material you want: ")
+        if id.lower()=="exit":
             break
-  return avail
+        elif int(id)>=len(avail) or int(id)<0:
+            print("Id not found.")
+            continue
+        else:
+            id=int(id)
+            i=id-1
+            while True:
+                amt=int(input("Enter how many you want to take: "))
+                if amt>avail[i]:
+                    print("Not enough.")
+                else:
+                    avail[i]-=amt
+                    break
+    return avail
+ 
 def add(avail):
-  while True:
-    id=input("Enter the id of the material you want: ")
-    if id.lower()=="exit":
-      break
-    else:
-      id=int(id)
-      i=id-1
-      amt=int(input("Enter how many you want to add: "))
-      avail[i]+=amt
-      break
-  return avail
+    while True:
+        id=input("Enter the id of the material you want: ")
+        if id.lower()=="exit":
+            break
+        elif int(id)>=len(avail) or int(id)<0:
+            print("Id not found.")
+            continue
+        else:
+            id=int(id)
+            i=id-1
+            amt=int(input("Enter how many you want to add: "))
+            avail[i]+=amt
+            break
+    return avail
 
 
 #Code
 while True:
-    command=input("Access/Take/Add: ")
-    if command.lower()=="access":
-        access(chem,avail)
-    elif command.lower()=="take":
-        take(avail)
-    elif command.lower()=="add":
-        add(avail)
-    elif command.lower()=="exit":
+    lab=input("Enter the lab you want to check or exit to leave: ")
+    if lab.lower()=="chem" or lab.lower()=="chemistry":
+        a=0
+    elif lab.lower()=="bio" or lab.lower()=="biology":
+        a=1
+    elif lab.lower()=="exit":
         break
+    else:
+        print("Invalid option")
+        continue
+    while True:
+        command=input("Access/Take/Add: ")
+        if command.lower()=="access":
+            access(materials[a],avail[a])
+        elif command.lower()=="take":
+            take(avail[a])
+        elif command.lower()=="add":
+            add(avail[a])
+        elif command.lower()=="exit":
+            break
+        else:
+            print("Invalid option")
+            continue
+    print("Thank you for checking on this lab.")
 print("Thank you for checking the logs.")
